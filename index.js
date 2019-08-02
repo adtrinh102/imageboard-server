5d588731203f203e150d303c93b53bdb2d6bbb5f
Duc Trinh <ducanhtrinh93@gmail.com>
2019-08-02 12:26:50 +0200

    Add image model

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
-const db = require('./db')
+const Image = require('./Image/model')
 
 app.listen(port, () => console.log(`Listening on: ${port}`))
