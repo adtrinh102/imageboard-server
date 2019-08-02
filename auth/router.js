b4a14fab8e1e16f76d3f4ba39fce6027a00d5eef
Duc Trinh <ducanhtrinh93@gmail.com>
2019-08-02 16:16:55 +0200

    Add User model & authorization checks

---
+++ auth/router.js
@@ -0,0 +1,73 @@
+const { Router } = require('express')
+const bcrypt = require('bcrypt')
+const { toJWT, toData } = require('./jwt')
+const User = require('../User/model')
+
+const router = new Router()
+
+router.post('/login', (req, res) => {
+    if (!req.body.email || !req.body.password) {
+        res.status(400).send({
+            message: 'Please supply a valid email and password'
+        })
+    }
+
+    else {
+        // 1. find user based on email address
+        User
+            .findOne({
+                where: {
+                    email: req.body.email
+                }
+            })
+            .then(entity => {
+                if (!entity) {
+                    res.status(400).send({
+                        message: 'User with that email does not exist'
+                    })
+                }
+
+                // 2. use bcrypt.compareSync to check the password against the stored hash
+                if (bcrypt.compareSync(req.body.password, entity.password)) {
+
+                    // 3. if the password is correct, return a JWT with the userId of the user (user.id)
+                    res.send({
+                        jwt: toJWT({ userId: entity.id })
+                    })
+                }
+                else {
+                    res.status(400).send({
+                        message: 'Password was incorrect'
+                    })
+                }
+            })
+            .catch(err => {
+                console.error(err)
+                res.status(500).send({
+                    message: 'Something went wrong'
+                })
+            })
+
+        // res.send({
+        //     jwt: toJWT({ userId: 1 })
+        // })
+    }
+})
+
+router.get('/secret-endpoint', (req, res) => {
+    const auth = req.headers.authorization && req.headers.authorization.split(' ')
+    if (auth && auth[0] === 'Bearer' && auth[1]) {
+        const data = toData(auth[1])
+        res.send({
+            message: 'Thanks for visiting the secret endpoint.',
+            data
+        })
+    }
+    else {
+        res.status(401).send({
+            message: 'Please supply some valid credentials'
+        })
+    }
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const authRouter = require('./auth/router')
 const imageRouter = require('./Image/router')
+const userRouter = require('./User/router')
+
 const app = express()
 const port = process.env.PORT || 4000
 
@@ -11,6 +14,8 @@ app.use(corsMiddleware)
 const parserMiddleware = bodyParser.json()
 app.use(parserMiddleware)
 
+app.use(authRouter)
 app.use(imageRouter)
+app.use(userRouter)
 
 app.listen(port, () => console.log(`Listening on: ${port}`))
