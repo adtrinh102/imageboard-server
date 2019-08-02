f37adc25953726031d1a277960ea5efbb24b846d
Duc Trinh <ducanhtrinh93@gmail.com>
2019-08-02 12:32:52 +0200

    Add image router and get method

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
-const Image = require('./Image/model')
+const imageRouter = require('./Image/router')
+
+app.use(imageRouter)
 
 app.listen(port, () => console.log(`Listening on: ${port}`))
